5c41f2d0cffe284968663ab29ad7af5b2cfc8419
Divyagash <sdivyagash@yahoo.com>
2017-01-26 13:17:18 -0500

    added feature Coverage

---
+++ R/featureCoverage.R
@@ -0,0 +1,32 @@
+#' Coverage statistics based on the feature
+#'
+#'Provides Coverage metrics for the sample by each features provided by the user
+#'@param takes bs object and list of features as input
+#'@return matrix a data frame with feature names and th number of CpG covered
+#'
+#'@example
+#'@export
+
+
+featureCoverage <-function(bs,features,genome){
+  if (!requireNamespace('annotatr',quietly=TRUE)){
+    stop("Pkg needed for this function to work. Please install it")
+  }
+  annotationFeatures<-c()
+  for (i in features){
+    annotationFeatures<-c(paste0(genome,'_',i),annotationFeatures)
+
+  }
+  annots_gr = annotatr::build_annotations(genome = genome, annotations = annotationFeatures)
+
+  # Intersect the regions with the reference annotations
+  dm_annotated = annotatr::annotate_regions(
+    regions = granges(bs),
+    annotations = annots_gr,
+    ignore.strand = TRUE,
+    quiet = TRUE)
+  sumAnnot<-annotatr::summarize_annotations(dm_annotated,quiet=TRUE)
+  sumAnnotDf<-as.data.frame(sumAnnot)
+  return(sumAnnotDf)
+
+}

74d9f4e8187a0d4932d5982b8cbec911c0f583f2
Divyagash <sdivyagash@yahoo.com>
2017-02-16 10:44:06 -0500

    edited the combine function

--- R/combineRDA.R
+++ R/combineRDA.R
@@ -7,20 +7,18 @@
 #'@param Takes bs object, name of the organism and reference genome
 #'@return Data frame with sample name and coverage in repeat masker regions
 #'@example
+#'combineRDA(c(bsObject1, bsObject2))
 #'@export
 
 
 combineRDA<-function(rdaList){
   message("Reading bsseq objects")
-  #bsList<-lapply(rdaList,function(file){
-  #  load(file)
-  #  bs
-  #})
+
 
   message("Combining bs object")
 
-  bs<-Reduce(combine,bsList)
-  bs@parameters<-bsList[[1]]@parameters
+  bs<-Reduce(combine,rdaList)
+  bs@parameters<-rdaList[[1]]@parameters
 
   #pData<-pData(bs)
   #pData$col<-as.numeric(factor(pData$type))

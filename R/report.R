87f13df2ed308c3948f9d9b3896b5c523fa59ad0
Divyagash <sdivyagash@yahoo.com>
2017-02-22 11:46:21 -0500

    Updated the qcreport

--- R/report.R
+++ R/report.R
@@ -4,6 +4,7 @@
 #'@param list of inputs
 #'@return A report will be the output based on rmarkdown file
 #'@example
+#'report(bsObject,'fileDirectory','Mus musculus','mm10')
 #'@export
 #
 
@@ -13,7 +14,9 @@ report <- function(bsObj,outdirectory,organism,genome,meta = NULL, cacheable = N
   #  outdirectory=getwd()
   #}
 
-  rmarkdown::render("./qcReport.Rmd",params=list(outdir=outdirectory,samples=bsObj,organism=organism,genome=genome),output_file=paste0(outdirectory,"/qcReport.html"))
+  #RmdFile<-file.path(PROJHOME,'scmeth/R','qcReport.Rmd')
+  RmdFile<-system.file(".",'qcReport.Rmd',package="scmeth")
+  rmarkdown::render(RmdFile,params=list(outdir=outdirectory,samples=bsObj,organism=organism,genome=genome),output_file=paste0(outdirectory,"/qcReport.html"))
 
   #return(23)
 }

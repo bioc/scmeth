05333069157994fc4b382987e5aec89a2a6aad51
Divyagash <sdivyagash@yahoo.com>
2017-03-08 12:48:37 -0500

    Added the density

---
+++ R/cpgDensity.R
@@ -0,0 +1,30 @@
+#'Provides Coverage metrics in the repeat masker region
+#'@param bs bsseq object
+#'@param organism scientific name of the organism of interest, i.e. Mus musculus or Homo sapiens
+#'@param genome reference alignment, i.e. mm10 or hg38
+#'@return Data frame with sample name and coverage in repeat masker regions
+#'@examples
+#'repMask(bsseqObject,'Mus musculus','mm10')
+#'@export
+
+
+cpgDensity<-function(bs,organism,windowLength=100){
+  if (organism == 'Mus musculus'){
+
+  library(BSgenome.Mmusculus.UCSC.mm10)
+  cov<-getCoverage(bs)
+  cpgd<-Repitools::cpgDensityCalc(granges(bs),Mmusculus,window = windowLength)
+  cpgdBin<-cut(cpgd,c(seq(0,20),max(cpgd)))
+  cpgdCov <- by(cov>0, cpgdBin, colSums)
+  cpgdCov <- do.call("rbind", cpgdCov)
+
+  }else if (organism == 'Homo sapiens'){
+    library(BSgenome.Hsapiens.UCSC.hg19)
+    cpgd<-Repitools::cpgDensityCalc(granges(bs),Hsapeins,window = windowLength)
+    cpgdBin<-cut(cpgd,c(seq(0,20),max(cpgd)))
+    cpgdCov <- by(cov>0, cpgdBin, colSums)
+    cpgdCov <- do.call("rbind", cpgdCov)
+
+  }
+  return(cpgdCov)
+}

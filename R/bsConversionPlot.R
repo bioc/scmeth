96f6c7d174d593c24f92482bf470eedbb477250d
Divyagash <sdivyagash@yahoo.com>
2017-04-02 17:32:23 -0400

    Updated the vignette

---
+++ R/bsConversionPlot.R
@@ -0,0 +1,24 @@
+#' Mehtylation bias plot
+#'
+#'Plot the bisulfite conversion rate for each sample
+#'based on the pheno data in the bs object
+#'
+#'@param Takes the bs object as the input
+#'@return Plot showing bisulfite conversion rate for each sample
+#'
+#'@examples
+#'bsConversionPlott(bs)
+#'@export
+
+
+bsConversionPlot<-function(bs){
+  phenoData<-pData(bs)
+  if ('bsconversion' %in% colnames(phenoData)) {
+    bscDf<-data.frame(sample=rownames(phenoData),bsc=phenoData$bsconversion)
+
+  g<-ggplot(meltedDf,aes(sample,bsc))+geom_point()+ylim(min(meltedDf$bsc)-0.05,max(meltedDf$bsc)+0.05)+theme(axis.text.x = element_text(angle = 60, hjust = 1))+
+    xlab('samples')+ylab('bisulfite conversion rate')+ggtitle('Bisulfite conversion rate across samples')
+  return(g)
+  }else
+    message("Provide a bs object with bisufite conversion to produce the plot")
+}
--- vignettes/my-vignette.R
+++ vignettes/my-vignette.R
@@ -1,4 +1,36 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  source("http://bioconductor.org/biocLite.R")
+#  biocLite("scmeth")
 
+## ---- eval=FALSE---------------------------------------------------------
+#  load('methylationData.rda')
+
+## ---- eval=FALSE---------------------------------------------------------
+#  scmeth::coverage(bs)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  scmeth::repmask(bs,"Mus musculus","mm10")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  scmeth::chromosomeCoverage(bs,"Mus musculus","mm10")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  scmeth::featureCoverage(bs,features=c('genes_exons','genes_introns','genes_intergenic','cpg_islands'),"mm10")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  scmeth::cpgDensity(bs,"Mus musculus",windowLength=1000)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  scmeth::downsample(bs)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  scmeth::mbiasplot(file)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  scmeth::methylationDist(file)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  scmeth::report(bs, '~/Documents',"Mus musculus","mm10")
 
 ## ---- fig.show='hold'----------------------------------------------------
 plot(1:10)

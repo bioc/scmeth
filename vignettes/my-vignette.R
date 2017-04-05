d1c438102187aa15a8bc54a9e6424507c02944ff
Divyagash <sdivyagash@yahoo.com>
2017-04-05 13:52:51 -0400

    Updated the vignette

--- vignettes/my-vignette.R
+++ vignettes/my-vignette.R
@@ -6,36 +6,48 @@
 #  load('methylationData.rda')
 
 ## ---- eval=FALSE---------------------------------------------------------
-#  scmeth::coverage(bs)
-
-## ---- eval=FALSE---------------------------------------------------------
-#  scmeth::repmask(bs,"Mus musculus","mm10")
-
-## ---- eval=FALSE---------------------------------------------------------
-#  scmeth::chromosomeCoverage(bs,"Mus musculus","mm10")
-
-## ---- eval=FALSE---------------------------------------------------------
-#  scmeth::featureCoverage(bs,features=c('genes_exons','genes_introns','genes_intergenic','cpg_islands'),"mm10")
-
-## ---- eval=FALSE---------------------------------------------------------
-#  scmeth::cpgDensity(bs,"Mus musculus",windowLength=1000)
-
-## ---- eval=FALSE---------------------------------------------------------
-#  scmeth::downsample(bs)
-
-## ---- eval=FALSE---------------------------------------------------------
-#  scmeth::mbiasplot(file)
-
-## ---- eval=FALSE---------------------------------------------------------
-#  scmeth::methylationDist(file)
-
-## ---- eval=FALSE---------------------------------------------------------
+#  library(scmeth)
 #  scmeth::report(bs, '~/Documents',"Mus musculus","mm10")
 
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ------------------------------------------------------------------------
+library(scmeth)
+load(system.file("extdata",'bsObject.rda',package='scmeth'))
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ------------------------------------------------------------------------
+scmeth::coverage(bs)
+
+## ---- warning=FALSE,message=FALSE----------------------------------------
+library(GenomicRanges)
+scmeth::repMask(bs,"Mus musculus","mm10")
+
+## ---- warning=FALSE------------------------------------------------------
+scmeth::chromosomeCoverage(bs)
+
+## ---- warning=FALSE,message=FALSE----------------------------------------
+scmeth::featureCoverage(bs,features=c('genes_exons','genes_introns','genes_intergenic','cpg_islands'),"mm10")
+
+## ----warning=FALSE,message=FALSE-----------------------------------------
+scmeth::cpgDensity(bs,"Mus musculus",windowLength=1000)
+
+## ----warning=FALSE-------------------------------------------------------
+scmeth::downsample(bs)
+
+## ----warning=FALSE,message=FALSE,fig.width=6,fig.height=6----------------
+library(ggplot2)
+scmeth::mbiasplot(system.file("extdata",'16_trimmed_sorted.txt',package='scmeth'))
+
+## ----warning=FALSE,message=FALSE,fig.width=6-----------------------------
+library(ggplot2)
+scmeth::methylationDist(bs,all=TRUE)
+
+## ------------------------------------------------------------------------
+scmeth::bsConversionPlot(bs)
+
+## ----warning=FALSE-------------------------------------------------------
+rda1<-createRDA(system.file("extdata",'sc-RRBS_zyg_01_chr1_CpG.bedGraph',package='scmeth'))
+rda2<-createRDA(system.file("extdata",'sc-RRBS_zyg_02_chr1_CpG.bedGraph',package='scmeth'))
+rda3<-createRDA(system.file("extdata",'sc-RRBS_zyg_03_chr1_CpG.bedGraph',package='scmeth'))
+
+## ------------------------------------------------------------------------
+combineRDA(c(rda1,rda2,rda3))
 
